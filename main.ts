e0a1e309c0ebbc61f4bc5fb6ccb67be4ac69bfb9
Kelvin Lee <yckelvin@users.noreply.github.com>
2023-12-12 23:43:49 +0800

    demo with light only

--- main.ts
+++ main.ts
@@ -10,7 +10,6 @@ microIoT.microIoT_showUserText(0, "INIT DEVICE")
 microIoT.microIoT_clear()
 basic.showNumber(2)
 microIoT.microIoT_showUserText(0, "Ready!")
-microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
 basic.forever(function () {
     light2 = pins.analogReadPin(AnalogPin.P0)
     microIoT.microIoT_showUserText(1, "Light: " + convertToText(light2))
